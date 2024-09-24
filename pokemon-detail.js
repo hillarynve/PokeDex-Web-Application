bc0f90da735a755d621d2695f3eb82030758cfd4
hillarynve <hillaryan@g.ucla.edu>
2024-09-24 15:49:25 -0700

    stat colors, type background - JavaScript

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -61,3 +61,128 @@ async function loadPokemon(id) {
     return false;
   }
 }
+
+async function navigatePokemon(id) {
+  await loadPokemon(id);
+}
+
+const typeColors = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  dark: "#EE99AC",
+};
+
+function setElementStyles(elements, cssProperty, value) {
+  elements.forEach((element) => {
+    element.style[cssProperty] = value;
+  })
+}
+
+function rgbaFromHex(hexColor) {
+  return [
+    parseInt(hexColor.slice(1,3), 16),
+    parseInt(hexColor.slice(3,5), 16),
+    parseInt(hexColor.slice(5,7), 16),
+  ].join(", ");
+}
+
+function setTypeBackgroundColor(pokemon) {
+  const mainType = pokemon.types[0].type.name;
+  const color = typeColors[mainType];
+
+  if (!color) {
+    console.warn(`Color not defined for type: ${mainType}`);
+    return;
+  }
+
+  const detailMainElement = document.querySelector(".detail-main");
+  setElementStyles([detailMainElement], "backgroundColor", color);
+  setElementStyles([detailMainElement], "borderColor", color);
+  setElementStyles(document.querySelectorAll("power-wrapper > p"), "backgroundColor", color);
+  setElementStyles(document.querySelectorAll(".stats-wrap p.stats"), "color", color);
+  setElementStyles(document.querySelectorAll(".stats-wrap .progress-bar"), "color", color);
+
+  const rgbaColor = rgbaFromHex(color);
+  const styleTag = document.createElement("style");
+  styleTag.innerHTML = `
+    .stats-wrap .progress-bar::-webkit-progress-bar {background-color: rgba(${rgbaColor}, 0.5);}
+    .stats-wrap .progress-bar::-webkit-progress-value {background-color: ${color};}
+  `;
+  document.head.appendChild(styleTag);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function createAndAppendElement(parent, tag, options = {}) {
+  const element = document.createElement(tag);
+  Object.keys(options).forEach((key) => {elemnent[key] = options[key];});
+  parent.appendChild(element);
+  return element;
+}
+
+function displayPokemonDetails(pokemon) {
+  const {name, id, types, weight, height, abilities, stats} = pokemon;
+  const capitalizePokemonName = capitalizeFirstLetter(name);
+  document.querySelector("title").textContent=capitalizePokemonName;
+  const detailMainElement = document.querySelector("detail-main");
+  detailMainElement.classList.add(name.toLowerCase());
+
+    document.querySelector("name-wrap .name").textContent = capitalizePokemonName;
+
+    document.querySelector(".pokemon-id-wrap .body2-fonts").textContent = `#${String(id).padStart(3, "0")}`;
+
+    const imageElement = document.querySelector(".detail-img-wrapper img");
+    imageElement.src = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/other/dream-world.${id}.svg`;
+
+    const typeWrapper = document.querySelector(".power-wrapper");
+    typeWrapper.innerHTML = "";
+    types.forEach(({ type }) => {
+      createAndAppendElement(typeWrapper, "p", {
+        className: `body3-fonts type ${type.name}`, textContent: type.name,
+      });
+    });
+
+    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.weight").textContent = `${weight / 10} kg`;
+    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.height").textContent = `${height / 10} kg`;
+
+    const abilitiesWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail.move");
+    abilities.forEach(({ability}) => {
+      createAndAppendElement(abilitiesWrapper, "p", {
+        className: "body3-fonts", textContent: ability.name,
+      });
+    });
+
+    const statNameMapping = {
+      hp: "hp",
+      attack: "ATK",
+      defence: "DEF",
+      "special-attack": "SATK",
+      "special-defence": "SDEF",
+      speed: "SPD",
+    };
+
+      stats.forEach(({stat, base_stat}) => {
+        const statDiv = document.createElement("div");
+        statDiv.className = "stats-wrap";
+        stats-typeWrapper.appendChild(statDiv);
+
+        createAndAppendElement
+      })
+} 
