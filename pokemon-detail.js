a4aef5e7d9db3ed934f5a48bc2fff1b0e6610827
hillarynve <hillaryan@g.ucla.edu>
2024-09-24 15:01:51 -0700

    load pokemon function complete - JavaScript

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -13,9 +13,47 @@ document.addEventListener("DOMContentLoaded", () => {
   loadPokemon(id);
 })
 
-
+// load pokemon's data
 async function loadPokemon(id) {
   try {
+    const [pokemon, pokemonSpecies] = await Promise.all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => 
+      res.json()
+    ),
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+      res.json()
+    ),
+  ]);
+
+  // clear previous pokemon's abilities
+  const abilitiesWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail.move");
+  abilitiesWrapper.innerHTML = "";
+
+  if (currentPokemonID === id) {
+    displayPokemonDetails(pokemon);
+    const flavorText = getEnglishFlavorText(pokemonSpecies);
+    document.querySelector(".body3-fonts.pokemon-description").textContent = flavorText;
+
+    const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((sel) => 
+    document.querySelector(sel)
+    );
+    leftArrow.removeEventListener("click", navigatePokemon);
+    rightArrow.removeEventListener("click", navigatePokemon);
+
+    if(id !== 1) {
+      leftArrow,addEventListener("click", () => {
+        navigatePokemon(id - 1);
+      });
+    }
+    if(id !== 151) {
+      rightArrow,addEventListener("click", () => {
+        navigatePokemon(id + 1);
+      });
+    }
+
+    // push intow window history & changes the URL without reloading the page
+    window.history.pushState({}, "", `./detail.html?id=${id}`);
+  }
+
     return true;
   }
   catch (error) {
